8d1afede99444bbc9adb73834edbee332300779b
Arman <armandwi.pangestu7@gmail.com>
2023-06-23 11:49:41 +0700

    latihan1

---
+++ latihan/latihan1_antara/app/src/main/java/com/example/latihan1_antara/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.latihan1_antara
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
