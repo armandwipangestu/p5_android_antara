3ed80a8e3c1d9434e7f89d91b1a0f3e9826ddcc8
Arman <armandwi.pangestu7@gmail.com>
2023-07-27 20:32:49 +0700

    UAS P5

--- latihan/latihan4_dashboard/app/src/main/java/com/example/latihan4_dashboard/MainActivity.java
+++ latihan/latihan4_dashboard/app/src/main/java/com/example/latihan4_dashboard/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.latihan4_dashboard;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,6 +43,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView pilih_menu = findViewById(R.id.pilih_menu);
+        Button btn_klik_bayar = findViewById(R.id.btn_klik_bayar);
+        final TextView bayar_menu = findViewById(R.id.bayar_menu);
+
+        btn_klik_bayar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String menu = pilih_menu.getText().toString();
+                bayar_menu.setText("Bayar: " + menu);
+            }
+        });
+
         bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
     }
